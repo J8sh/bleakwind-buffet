7ca499c50d34966e2718e34f318b79f02e0bb639
j8sh <j8shigarcia@aol.com>
2020-09-13 03:51:53 -0500

    Saving progress

---
+++ Data/Menu.cs
@@ -0,0 +1,30 @@
+﻿/*
+* Author: Joshua Garcia
+* Class name: Menu.cs
+* Purpose: Static file used to represent the static methods of entrees(), Sides(), Drinks()
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Sides;
+using BleakwindBuffet.Data.Enums;
+
+namespace BleakwindBuffet.Data
+{
+    /// <summary>
+    /// A static class for all the foods and drinks on the menu
+    /// </summary>
+    public static class Menu
+    {
+
+        static IEnumerable<IOrderItem> Entrees()
+        {
+            return ;
+        }
+        
+    }
+}
